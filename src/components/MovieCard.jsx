2b5e9d61e937acfc39f3b622768a6fa157234803
Ilham Syauqi <ilhamsyauqih@outlook.co.id>
2026-05-29 22:14:15 +0700

    feat: initial init

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,4 @@
+// ✅ BELAJAR PROPS: komponen ini menerima data dari luar
+const MovieCard = () => {
+    
+}
---
+++ src/data/movies.js
@@ -0,0 +1,36 @@
+const movies = [
+  {
+    id: 1,
+    title: "Interstellar",
+    genre: "Sci-Fi",
+    rating: 9.2,
+    status: "Sudah nonton",
+    year: 2014,
+  },
+  {
+    id: 2,
+    title: "Spirited Away",
+    genre: "Animasi",
+    rating: 9.5,
+    status: "Sudah nonton",
+    year: 2001,
+  },
+  {
+    id: 3,
+    title: "Dune: Part Two",
+    genre: "Sci-Fi",
+    rating: 8.7,
+    status: "Mau nonton",
+    year: 2024,
+  },
+  {
+    id: 4,
+    title: "Parasite",
+    genre: "Thriller",
+    rating: 9.1,
+    status: "Sudah nonton",
+    year: 2019,
+  },
+];
+
+export default movies;
