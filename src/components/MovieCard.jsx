8e9ebf9155325f5fcec552cc46df792081ccc5bf
Ilham Syauqi <ilhamsyauqih@outlook.co.id>
2026-05-30 09:30:26 +0700

    feat: add movie card components

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,13 @@
 // ✅ BELAJAR PROPS: komponen ini menerima data dari luar
-const MovieCard = () => {
-    
+const MovieCard = ({title, genre, rating, status, year}) => {
+    return (
+        <div className="MovieCard">
+            <h3>{title}</h3>
+            <p>{genre} . {year}</p>
+            <p>⭐ {rating}</p>
+            <span>{status}</span>
+        </div>
+    );
 }
+
+export default MovieCard;
