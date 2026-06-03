26472222bf2d4f6040fb75f972ae5581a5f20bc0
Ilham Syauqi <ilhamsyauqih@outlook.co.id>
2026-06-03 23:02:58 +0700

    Menambahkan navigasi bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import NavigationBar from './components/NavigationBar';
+import "./style/landingPage.css"
 
 function App() {
   return (
     <div>
-      <h1>TESTING</h1>
+      {/* Intro Section */}
+      <div className='myBG'>
+      <NavigationBar />
+      </div>
+      {/* Intro Section End */}
     </div>
   );
 }
