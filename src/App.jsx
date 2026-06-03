86a053b4b7ae7187206676729ef3504a9271a525
Ilham Syauqi <ilhamsyauqih@outlook.co.id>
2026-06-03 22:33:38 +0700

    update inisialisasi awal kembali

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
 import { useState } from 'react'
-import allMovies from "./data/movies"
-import MovieList from "./components/MovieList"
-import MovieCard from './components/MovieCard'
-import './App.css'
-import FilterBar from './components/FilterBar'
 
 function App() {
-  const [activeFilter, setActiveFilter] = useState("Semua");
-
-  const filteredMovies = activeFilter === "Semua"
-  ? allMovies
-  : allMovies.filter((m) => m.genre === activeFilter);
-
   return (
     <div>
-      <h1>Katalog Film Saya</h1>
-
-      <FilterBar 
-      activeFilter={activeFilter}
-      onFilterChange={setActiveFilter}
-      />
-
-      <MovieList movies={filteredMovies} />
+      <h1>TESTING</h1>
     </div>
-  )
+  );
 }
 
 export default App
